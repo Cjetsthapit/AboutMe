0d358311a50f0bad4562c9a7a6277ef9fccc0377
Srijeet Sthapit <48915916+Cjetsthapit@users.noreply.github.com>
2023-09-06 10:12:47 -0400

    Initial Push

---
+++ App.js
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>About Me</Text>
+      <Text>Srijeet Sthapit</Text>
+      <Text>ID: 301365217</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title:{
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginBottom: 30
+  }
+});
